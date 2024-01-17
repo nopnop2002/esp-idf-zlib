c3c6b1e985f946d62dd88e96823a6192737513a7
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-01-17 17:32:17 +0900

    Separate compression and decompression as tasks

--- file/README.md
+++ file/README.md
@@ -12,17 +12,25 @@ I used [this](https://www.zlib.net/zpipe.c) as a reference.
 
 # Screen Shot   
 ```
-I (400) MAIN: Initializing SPIFFS
-I (430) MAIN: Partition size: total: 354161, used: 1757
-I (440) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417 ---> Original file
-I (490) MAIN: comp=0
-I (500) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417
-I (500) printDirectory: /root d_name=README.txt.zip d_ino=0 fsize=595 ---> Compressed file
-I (550) MAIN: decomp=0
-I (560) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417
-I (560) printDirectory: /root d_name=README.txt.zip d_ino=0 fsize=595
-I (560) printDirectory: /root d_name=README.txt.txt d_ino=0 fsize=1417 ---> DeCompressed file
-I (580) MAIN: FLASH unmounted
-I (580) main_task: Returned from app_main()
+I (442) MAIN: Initializing SPIFFS
+I (462) MAIN: Partition size: total: 354161, used: 1757
+I (462) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417 ---> Original file
+I (472) COMPRESS: Start
+I (472) COMPRESS: param.srcPath=[/root/README.txt]
+I (472) COMPRESS: param.dstPath=[/root/README.txt.zip]
+I (522) COMPRESS: Finish
+I (522) MAIN: comp_result=0
+I (522) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417
+I (522) printDirectory: /root d_name=README.txt.zip d_ino=0 fsize=599 ---> Compressed file
+I (542) DECOMPRESS: Start
+I (542) DECOMPRESS: param.srcPath=[/root/README.txt.zip]
+I (552) DECOMPRESS: param.dstPath=[/root/README.txt.txt]
+I (592) DECOMPRESS: Finish
+I (592) MAIN: decomp_result=0
+I (592) printDirectory: /root d_name=README.txt d_ino=0 fsize=1417
+I (592) printDirectory: /root d_name=README.txt.zip d_ino=0 fsize=599
+I (602) printDirectory: /root d_name=README.txt.txt d_ino=0 fsize=1417 ---> DeCompressed file
+I (622) MAIN: FLASH unmounted
+I (622) main_task: Returned from app_main()
 ```
 
--- file/main/main.c
+++ file/main/main.c
@@ -13,30 +13,24 @@
 #include <sys/unistd.h>
 #include <sys/stat.h>
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_vfs.h"
 #include "nvs_flash.h"
-#include "esp_vfs_fat.h"
 #include "esp_spiffs.h"
 #include "zlib.h"
 
-#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0))
-#define esp_vfs_fat_spiflash_mount esp_vfs_fat_spiflash_mount_rw_wl
-#define esp_vfs_fat_spiflash_unmount esp_vfs_fat_spiflash_unmount_rw_wl
-#endif
+#include "parameter.h"
 
 static const char *TAG = "MAIN";
 
 esp_err_t mountSPIFFS(char * partition_label, char * mount_point) {
 	ESP_LOGI(TAG, "Initializing SPIFFS");
 	esp_vfs_spiffs_conf_t conf = {
-		//.base_path = "/spiffs",
 		.base_path = mount_point,
-		//.partition_label = NULL,
 		.partition_label = partition_label,
 		.max_files = 4, // maximum number of files which can be open at the same time
-		//.max_files = 256,
 		.format_if_mount_failed = true
 	};
 
@@ -64,26 +58,6 @@ esp_err_t mountSPIFFS(char * partition_label, char * mount_point) {
 		ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
 	}
 	return ret;
-}
-
-wl_handle_t mountFAT(char * partition_label, char * mount_point) {
-	ESP_LOGI(TAG, "Initializing FLASH file system");
-	// To mount device we need name of device partition, define base_path
-	// and allow format partition in case if it is new one and was not formated before
-	const esp_vfs_fat_mount_config_t mount_config = {
-		.format_if_mount_failed = true,
-		.max_files = 4, // maximum number of files which can be open at the same time
-		.allocation_unit_size = CONFIG_WL_SECTOR_SIZE
-	};
-	wl_handle_t s_wl_handle;
-	esp_err_t err = esp_vfs_fat_spiflash_mount(mount_point, partition_label, &mount_config, &s_wl_handle);
-	if (err != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to mount FLASH (%s)", esp_err_to_name(err));
-		return -1;
-	}
-	ESP_LOGI(TAG, "Mount FAT filesystem on %s", mount_point);
-	ESP_LOGI(TAG, "s_wl_handle=%"PRIi32, s_wl_handle);
-	return s_wl_handle;
 }
 
 static fpos_t getFileSize(char *fullPath) {
@@ -124,137 +98,8 @@ static void printDirectory(char * path) {
 	closedir(dir);
 }
 
-// I used this code.
-// https://www.zlib.net/zpipe.c
-
-//#define CHUNK 16384
-#define CHUNK 2048
-
-/* Compress from file source to file dest until EOF on source.
-   comp() returns Z_OK on success, Z_MEM_ERROR if memory could not be
-   allocated for processing, Z_STREAM_ERROR if an invalid compression
-   level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
-   version of the library linked do not match, or Z_ERRNO if there is
-   an error reading or writing the files. */
-int comp(FILE *source, FILE *dest, int level)
-{
-	int ret, flush;
-	unsigned have;
-	z_stream strm;
-	unsigned char in[CHUNK];
-	unsigned char out[CHUNK];
-
-	/* allocate deflate state */
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	ret = deflateInit(&strm, level);
-	if (ret != Z_OK) {
-		ESP_LOGI(__FUNCTION__, "deflateInit fail [%d]", ret);
-		return ret;
-	}
-
-	/* compress until end of file */
-	do {
-		strm.avail_in = fread(in, 1, CHUNK, source);
-		if (ferror(source)) {
-			(void)deflateEnd(&strm);
-			ESP_LOGI(__FUNCTION__, "fread fail");
-			return Z_ERRNO;
-		}
-		flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
-		strm.next_in = in;
-
-		/* run deflate() on input until output buffer not full, finish
-		   compression if all of source has been read in */
-		do {
-			strm.avail_out = CHUNK;
-			strm.next_out = out;
-			ret = deflate(&strm, flush);	/* no bad return value */
-			assert(ret != Z_STREAM_ERROR);	/* state not clobbered */
-			have = CHUNK - strm.avail_out;
-			if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-				(void)deflateEnd(&strm);
-				ESP_LOGI(__FUNCTION__, "fwrite fail");
-				return Z_ERRNO;
-			}
-		} while (strm.avail_out == 0);
-		assert(strm.avail_in == 0);		/* all input will be used */
-
-		/* done when last data in file processed */
-	} while (flush != Z_FINISH);
-	assert(ret == Z_STREAM_END);		/* stream will be complete */
-
-	/* clean up and return */
-	(void)deflateEnd(&strm);
-	return Z_OK;
-}
-
-/* Decompress from file source to file dest until stream ends or EOF.
-   decomp() returns Z_OK on success, Z_MEM_ERROR if memory could not be
-   allocated for processing, Z_DATA_ERROR if the deflate data is
-   invalid or incomplete, Z_VERSION_ERROR if the version of zlib.h and
-   the version of the library linked do not match, or Z_ERRNO if there
-   is an error reading or writing the files. */
-int decomp(FILE *source, FILE *dest)
-{
-	int ret;
-	unsigned have;
-	z_stream strm;
-	unsigned char in[CHUNK];
-	unsigned char out[CHUNK];
-
-	/* allocate inflate state */
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	strm.avail_in = 0;
-	strm.next_in = Z_NULL;
-	ret = inflateInit(&strm);
-	if (ret != Z_OK) {
-		ESP_LOGI(__FUNCTION__, "inflateInit fail [%d]", ret);
-		return ret;
-	}
-
-	/* decompress until deflate stream ends or end of file */
-	do {
-		strm.avail_in = fread(in, 1, CHUNK, source);
-		if (ferror(source)) {
-			(void)inflateEnd(&strm);
-			return Z_ERRNO;
-		}
-		if (strm.avail_in == 0)
-			break;
-		strm.next_in = in;
-
-		/* run inflate() on input until output buffer not full */
-		do {
-			strm.avail_out = CHUNK;
-			strm.next_out = out;
-			ret = inflate(&strm, Z_NO_FLUSH);
-			assert(ret != Z_STREAM_ERROR);	/* state not clobbered */
-			switch (ret) {
-			case Z_NEED_DICT:
-				ret = Z_DATA_ERROR;		/* and fall through */
-			case Z_DATA_ERROR:
-			case Z_MEM_ERROR:
-				(void)inflateEnd(&strm);
-				return ret;
-			}
-			have = CHUNK - strm.avail_out;
-			if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-				(void)inflateEnd(&strm);
-				return Z_ERRNO;
-			}
-		} while (strm.avail_out == 0);
-
-		/* done when inflate() says it's done */
-	} while (ret != Z_STREAM_END);
-
-	/* clean up and return */
-	(void)inflateEnd(&strm);
-	return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-}
+void comp_task(void *pvParameters);
+void decomp_task(void *pvParameters);
 
 void app_main(void)
 {
@@ -268,40 +113,34 @@ void app_main(void)
 	}
 	ESP_ERROR_CHECK(ret);
 
-	// Mount FAT File System on FLASH
 	// Mount SPIFFS File System on FLASH
 	char *mount_point = "/root";
 	char *partition_label = "storage1";
-	ret = mountSPIFFS(partition_label, mount_point);
-	if (ret != ESP_OK) {
-		while(1) { vTaskDelay(1); }
-	};
+	ESP_ERROR_CHECK(mountSPIFFS(partition_label, mount_point));
 
 	// Compress
 	printDirectory(mount_point);
-	FILE* source = fopen("/root/README.txt", "r");
-	FILE* dest = fopen("/root/README.txt.zip", "w");
-	int result = comp(source, dest, Z_DEFAULT_COMPRESSION);
-	ESP_LOGI(TAG, "comp=%d", result);
-	fclose(source);
-	fclose(dest);
+	PARAMETER_t param;
+	param.ParentTaskHandle =  xTaskGetCurrentTaskHandle();
+	strcpy(param.srcPath, "/root/README.txt");
+	strcpy(param.dstPath, "/root/README.txt.zip");
+	param.level = Z_DEFAULT_COMPRESSION;
+	UBaseType_t priority = uxTaskPriorityGet(NULL);
+	xTaskCreate(comp_task, "COMPRESS", 1024*6, (void *)&param, priority, NULL);
+	uint32_t comp_result = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+	ESP_LOGI(TAG, "comp_result=%"PRIi32, comp_result);
+	if (comp_result != 0) vTaskDelete(NULL);
 	printDirectory(mount_point);
 
 	// Decompress
-	source = fopen("/root/README.txt.zip", "r");
-	dest = fopen("/root/README.txt.txt", "w");
-	result = decomp(source, dest);
-	ESP_LOGI(TAG, "decomp=%d", result);
-	fclose(source);
-	fclose(dest);
+	strcpy(param.srcPath, "/root/README.txt.zip");
+	strcpy(param.dstPath, "/root/README.txt.txt");
+	xTaskCreate(decomp_task, "DECOMPRESS", 1024*6, (void *)&param, priority, NULL);
+	uint32_t decomp_result = ulTaskNotifyTake( pdTRUE, portMAX_DELAY );
+	ESP_LOGI(TAG, "decomp_result=%"PRIi32, decomp_result);
 	printDirectory(mount_point);
 
-#if 0
-	// Unmount FAT file system
-	esp_vfs_fat_spiflash_unmount(mount_point, s_wl_handle);
-#endif
-	// Unmount SPIFFS file system
+	// Unmount SPIFFS File System
 	esp_vfs_spiffs_unregister(partition_label);
 	ESP_LOGI(TAG, "FLASH unmounted");
-
 }
