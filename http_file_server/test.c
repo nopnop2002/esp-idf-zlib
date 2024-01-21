84d19d7c9c2a7799ddcfe9c7d579d968d9e203db
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-01-21 10:08:16 +0900

    moved linux sources

--- http_file_server/test.c
+++
@@ -1,8 +0,0 @@
-// test.c
-#include <zlib.h>
-int main(void) {
-  z_stream strm = { .zalloc = Z_NULL, .zfree = Z_NULL, .opaque = Z_NULL };
-  deflateInit(&strm, Z_DEFAULT_COMPRESSION);
-  deflateEnd(&strm);
-  return 0;
-}
